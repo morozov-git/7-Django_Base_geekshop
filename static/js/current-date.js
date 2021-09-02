1227c7fa54a48e3a83f3f17f9419b2386fe04b09
morozov.da <morozov.da@gmail.com>
2021-09-02 04:02:11 +0300

    Added index-products base(blocks) + current date

--- static/js/current-date.js
+++ static/js/current-date.js
@@ -2,5 +2,9 @@
 // <a class="nav-link" style="color: darkgray" id="current_date_time_block" ></a>
 
 "use strict"
-let date = new Date().toLocaleDateString()
-document.getElementById('current_date_time_block').innerText = "Сегодня: " + date + "   ";
+let date = " " + new Date().toLocaleDateString()
+let calendar = document.querySelector("#current_date_time_block");
+calendar.insertAdjacentHTML('afterend', date)
+//document.getElementsByClassName("far fa-calendar")[0].parentNode.innerText += date;
+// document.getElementById('current_date_time_block').innerText = date;
+//document.getElementById('current_date_time_block').innerText = "Сегодня: " + date + "   ";
