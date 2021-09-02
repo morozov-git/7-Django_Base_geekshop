39caaca2e42707e6fab3c8abc7375214322053d5
morozov.da <morozov.da@gmail.com>
2021-09-02 07:04:12 +0300

    Added products.json update products.html

--- products/views.py
+++ products/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
+import json
+
+with open("static/products.json", "r", encoding="utf-8") as goods:
+	products_list = json.load(goods)
 
 
 # Create your views here.
 def index(request):
-	context = {
-		"title": "GeekShop",
-	}
+	context = {"title": "geek shop"}
 	return render(request, "index.html", context)
 
 
 def products(request):
 	context = {
-		"title": "GeekShop_Catalog",
+		"title": "geek shop catalog",
+		"products_list": products_list
 	}
 	return render(request, "products.html", context)
 
@@ -33,7 +36,7 @@ def test(request):
 		"user": "UserName",
 		"description": "Hello world!",
 		# "test": test1(),
-		"prodicts": [
+		"products": [
 			{"name": "Худи черного цвета с монограммами adidas Originals 111", "price": 6091},
 			{"name": "Худи черного цвета с монограммами adidas Originals 222", "price": 6092}
 		],
@@ -42,6 +45,6 @@ def test(request):
 	}
 	return render(request, "test.html", context)
 
-
 # def discount(price):
 # 	return price-(price/100)*25
+# with open("templates/products.json", "r", encoding="ut8-8") as goods:
