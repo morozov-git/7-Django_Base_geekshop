7998ee6fe2d8edcc1092fdd5f23968af982898d5
morozov.da <morozov.da@gmail.com>
2021-09-05 03:02:43 +0300

    Create Admin, update models(products)

--- products/models.py
+++ products/models.py
@@ -7,6 +7,10 @@ class ProductsCategory(models.Model):
 	name = models.CharField(max_length=64)
 	description = models.TextField(blank=True, null=True)
 
+	def __str__(self):
+		return self.name
+
+
 
 class Product(models.Model):
 	name = models.CharField(max_length=128)
@@ -15,3 +19,6 @@ class Product(models.Model):
 	price = models.DecimalField(max_digits=8, decimal_places=2)
 	quantity = models.PositiveIntegerField(default=0)
 	category = models.ForeignKey(ProductsCategory, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f"{self.name} | {self.category}"
