e754a9ec1b72c46366ac58f1784c26cbe0959af3
morozov.da <morozov.da@gmail.com>
2021-09-06 04:23:58 +0300

    added - loading ProductsCategory from the DB

--- products/views.py
+++ products/views.py
@@ -7,7 +7,7 @@ from products.models import Product, ProductsCategory
 # with open("static/products.json", "r", encoding="utf-8") as goods:
 # 	products_list = json.load(goods)
 products_list = Product.objects.all()
-
+categories_list = ProductsCategory.objects.all()
 
 # Create your views here.
 def index(request):
@@ -18,7 +18,8 @@ def index(request):
 def products(request):
 	context = {
 		"title": "geek shop catalog",
-		"products_list": products_list
+		"products_list": products_list,
+		"categories_list": categories_list
 	}
 	return render(request, "products.html", context)
 
