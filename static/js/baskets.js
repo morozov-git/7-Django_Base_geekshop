f0a41919b089907b4db74f56085bdf0ec4a25dce
morozov.da <morozov.da@gmail.com>
2021-10-15 02:30:54 +0300

    fixed products, baskets and orders script errors

--- static/js/baskets.js
+++ static/js/baskets.js
@@ -15,17 +15,17 @@ window.onload = function () {
     // Event.preventDefault();
     })
 
-    $('.product_list').on('click', 'button[class="btn btn-outline-success"]', function (){
-        let but_href = event.target;
-        // console.log(but_href);
-        // console.log(but_href.name);
-
-        $.ajax({
-            url : '/baskets/baskets_add/' + but_href.name,
-            success: function (data){
-                $('.basket_icon').html(data.result_icon)
-            },
-        });
-    })
+    // $('.product_list').on('click', 'button[class="btn btn-outline-success"]', function (){
+    //     let but_href = event.target;
+    //     // console.log(but_href);
+    //     // console.log(but_href.name);
+    //
+    //     $.ajax({
+    //         url : '/baskets/baskets_add/' + but_href.name,
+    //         success: function (data){
+    //             $('.basket_icon').html(data.result_icon)
+    //         },
+    //     });
+    // })
 
 }
