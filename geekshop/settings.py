766ae05a29cefef5b7283256bb3fa104fdd7b997
morozov.da <morozov.da@gmail.com>
2021-10-16 00:14:49 +0300

    settings update

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from django.template.backends import django
 from dotenv import load_dotenv
 from pathlib import Path
 import os
@@ -149,10 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-    /home/django/7-Django_Base_geekshop/static/,
-]
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
