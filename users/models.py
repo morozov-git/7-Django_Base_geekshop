6e0ae529f73d799d10ca23bba4062ac507f540e3
morozov.da <morozov.da@gmail.com>
2021-10-15 19:08:49 +0300

    del migrations

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class User(AbstractUser):
 	image = models.ImageField(upload_to='user_image', blank=True)
 	age = models.PositiveIntegerField(verbose_name='возраст', default=18)
 
-	activation_key = models.CharField(max_length=128, blank=True)
+	activation_key = models.CharField(max_length=500, blank=True)
 	# activation_key_expires = models.DateTimeField(default=(now()+timedelta(hours=48)))
 	activation_key_expires = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
@@ -33,9 +33,9 @@ class UserProfile(models.Model):
 	)
 
 	user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
-	tagline = models.CharField(verbose_name='теги', max_length=128, blank=True)
+	tagline = models.CharField(verbose_name='теги', max_length=500, blank=True)
 	about_me = models.TextField(verbose_name='о себе', blank=True)
-	gender = models.CharField(verbose_name='пол', choices=GENDER_CHOICES, max_length=4, blank=True)
+	gender = models.CharField(verbose_name='пол', choices=GENDER_CHOICES, max_length=10, blank=True)
 
 	@receiver(post_save, sender=User)
 	def create_user_profile(sender, instance, created, **kwargs):
