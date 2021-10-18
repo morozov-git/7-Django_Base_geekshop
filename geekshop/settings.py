2ed9e5c648af0c6834ff261956aa54695058b51b
morozov.da <morozov.da@gmail.com>
2021-10-18 15:02:47 +0300

    commit

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -17,6 +17,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR: Union[Path, Any] = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -49,6 +50,8 @@ INSTALLED_APPS = [
 	'admins',
 	'social_django',
 	'ordersapp',
+	'debug_toolbar',
+	'template_profiler_panel',
 
 ]
 
@@ -61,6 +64,8 @@ MIDDLEWARE = [
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'social_django.middleware.SocialAuthExceptionMiddleware',
+	'debug_toolbar.middleware.DebugToolbarMiddleware',
+
 ]
 
 ROOT_URLCONF = 'geekshop.urls'
@@ -143,10 +148,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (BASE_DIR / 'static',)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (
+# 	os.path.join(BASE_DIR, 'assets'),
+# )
+# STATICFILES_FINDERS = (
+# 	'django.contrib.staticfiles.finders.FileSystemFinder',
+# 	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -204,3 +215,28 @@ SOCIAL_AUTH_PIPELINE = (
 	'social_core.pipeline.social_auth.load_extra_data',
 	'social_core.pipeline.user.user_details',
 )
+
+
+if DEBUG:
+	def show_toolbar(request):
+		return True
+	DEBUG_TOOLBAR_CONFIG = {
+		'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+	}
+
+	DEBUG_TOOLBAR_PANELS = [
+		'debug_toolbar.panels.versions.VersionsPanel',
+		'debug_toolbar.panels.timer.TimerPanel',
+		'debug_toolbar.panels.settings.SettingsPanel',
+		'debug_toolbar.panels.headers.HeadersPanel',
+		'debug_toolbar.panels.request.RequestPanel',
+		'debug_toolbar.panels.sql.SQLPanel',
+		'debug_toolbar.panels.templates.TemplatesPanel',
+		'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+		'debug_toolbar.panels.cache.CachePanel',
+		'debug_toolbar.panels.signals.SignalsPanel',
+		'debug_toolbar.panels.logging.LoggingPanel',
+		'debug_toolbar.panels.redirects.RedirectsPanel',
+		'debug_toolbar.panels.profiling.ProfilingPanel',
+		'template_profiler_panel.panels.template.TemplateProfilerPanel',
+	]
