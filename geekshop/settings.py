04405c134fda8e508b63c2538114003a26a9d0f4
morozov.da <morozov.da@gmail.com>
2021-10-18 03:04:39 +0300

    added settings for social

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -192,6 +192,9 @@ SOCIAL_AUTH_VK_OAUTH2_IGNORE_DEFAULT_SCORE = True
 SOCIAL_AUTH_VK_OAUTH2_SCORE = ['email']
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True # проверить авторизацию через другие аккаунты
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
+
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
