78c1c26f989ae2e9464bc651ef62a40631fe383a
morozov.da <morozov.da@gmail.com>
2021-10-20 21:24:29 +0300

    low cash

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
 
 ]
 
+
 ROOT_URLCONF = 'geekshop.urls'
 
 TEMPLATES = [
@@ -96,21 +97,21 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
-		# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	}
-}
-
 # DATABASES = {
 # 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql',
-# 		'NAME': 'geekshop',
-# 		'USER': 'postgres',
+# 		'ENGINE': 'django.db.backends.sqlite3',
+# 		'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+# 		# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 # 	}
 # }
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'geekshop',
+		'USER': 'postgres',
+	}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -149,8 +150,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (BASE_DIR / 'static',)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = (
 # 	os.path.join(BASE_DIR, 'assets'),
@@ -243,3 +244,15 @@ if DEBUG:
 		'debug_toolbar.panels.profiling.ProfilingPanel',
 		'template_profiler_panel.panels.template.TemplateProfilerPanel',
 	]
+
+
+# if os.name == 'posix':
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 120
+CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+CACHES = {'default':
+	{'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+	'LOCATION': '127.0.0.1:11211', }
+}
+LOW_CACHE = True
+
