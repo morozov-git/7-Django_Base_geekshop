55797a9842cae79e707153aed8d3cde9442b6abe
morozov.da <morozov.da@gmail.com>
2021-10-21 00:30:40 +0300

    сached + category

--- products/views.py
+++ products/views.py
@@ -38,27 +38,40 @@ def get_links_category():
 	else:
 		return ProductsCategory.objects.filter(is_active=True)
 
-def get_links_product(cat_id=0):
+# def get_links_product(cat_id=0):
+# 	if settings.LOW_CACHE:
+# 		key = 'links_product' + str(cat_id)
+# 		links_product = cache.get(key)
+#
+# 		if links_product is None and cat_id != 0:
+# 			links_product = Product.objects.filter(is_active=True).filter(category_id=cat_id).select_related()
+# 			# links_product = Product.objects.filter(is_active=True).select_related()
+# 			cache.set(key, links_product)
+# 		else:
+# 			links_product = Product.objects.filter(is_active=True).select_related()
+# 			# links_product = Product.objects.filter(is_active=True).select_related()
+# 			cache.set(key, links_product)
+# 		return links_product
+# 	else:
+# 		if cat_id != 0:
+# 			# return Product.objects.filter(is_active=True).select_related()
+# 			return Product.objects.filter(is_active=True).filter(category_id=cat_id).select_related()
+# 		else:
+# 			return Product.objects.filter(is_active=True).select_related()
+
+def get_links_product():
 	if settings.LOW_CACHE:
 		key = 'links_product' + str(cat_id)
 		links_product = cache.get(key)
 
-		if links_product is None and cat_id != 0:
-			links_product = Product.objects.filter(is_active=True).filter(category_id=cat_id).select_related()
-			# links_product = Product.objects.filter(is_active=True).select_related()
-			cache.set(key, links_product)
-		else:
+		if links_product is None:
 			links_product = Product.objects.filter(is_active=True).select_related()
 			# links_product = Product.objects.filter(is_active=True).select_related()
 			cache.set(key, links_product)
 		return links_product
 	else:
-		if cat_id != 0:
 			# return Product.objects.filter(is_active=True).select_related()
-			return Product.objects.filter(is_active=True).filter(category_id=cat_id).select_related()
-		else:
 			return Product.objects.filter(is_active=True).select_related()
-
 
 
 def index(request):
@@ -73,18 +86,17 @@ def index(request):
 def products(request, cat_id=0, page=1):
 	# categories_list = ProductsCategory.objects.all()
 	# baskets = basket_icon(request) # после подключения контекстного процессора можно отключить
-	products_list = get_links_product(cat_id)
-	# if cat_id == 0:
-	# 	# print(cat_id)
-	# 	# products_list = Product.objects.all().select_related('category')
-	# 	# products_list = get_links_product()  # для кэширования списка продуктов вызываем дополнительный метод
-	# 	products_list = get_links_product(cat_id) # для кэширования списка продуктов вызываем дополнительный метод
-	# else:
-	# 	# print(cat_id)
-	# 	# products_list = Product.objects.filter(category_id=cat_id)
-	# 	# products_list = Product.objects.filter(category_id=cat_id).select_related('category')
-	# 	products_list = get_links_product(cat_id)  # для кэширования списка продуктов вызываем дополнительный метод
-	# 	# print(products_list.query)
+	if cat_id == 0:
+		# print(cat_id)
+		# products_list = Product.objects.all().select_related('category')
+		# products_list = get_links_product()  # для кэширования списка продуктов вызываем дополнительный метод
+		products_list = get_links_product() # для кэширования списка продуктов вызываем дополнительный метод
+	else:
+		# print(cat_id)
+		# products_list = Product.objects.filter(category_id=cat_id)
+		products_list = Product.objects.filter(category_id=cat_id).select_related('category')
+		# products_list = get_links_product(cat_id)  # для кэширования списка продуктов вызываем дополнительный метод
+		# print(products_list.query)
 
 	paginator = Paginator(products_list, per_page=3) # количество товаров на странице
 	try:
