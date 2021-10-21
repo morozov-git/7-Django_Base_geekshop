8e98d581826498d3ca2a5d02d926c20cf93951ad
morozov.da <morozov.da@gmail.com>
2021-10-21 19:35:11 +0300

    сached in middleware

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -157,10 +157,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = (
 # 	os.path.join(BASE_DIR, 'assets'),
 # )
-# STATICFILES_FINDERS = (
-# 	'django.contrib.staticfiles.finders.FileSystemFinder',
-# 	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# )
+STATICFILES_FINDERS = (
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
