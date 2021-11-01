bb27f6134c81308b8aaa569aa53003b60ccc7240
morozov.da <morozov.da@gmail.com>
2021-11-01 16:59:02 +0300

    admin

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from .models import ProductsCategory, Product
-
-
+from ordersapp.models import Order
 # Register your models here.
 admin.site.register(ProductsCategory)
+
+
 # admin.site.register(Product)
 
 @admin.register(Product)
 class Product(admin.ModelAdmin):
-
 	list_display = ('name', 'price', 'quantity', 'category')
 	fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
 	# readonly_fields = ('description')
