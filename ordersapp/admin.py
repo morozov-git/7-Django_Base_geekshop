0db35f34a16437c1c52e716fb130edf34fad9d70
morozov.da <morozov.da@gmail.com>
2021-11-01 05:40:11 +0300

    admin

--- ordersapp/admin.py
+++ ordersapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from ordersapp.models import Order
+
+
+
+# @admin.register(Basket)
+class OrderAdmin(admin.TabularInline):
+	model = Order
+	fields = ('Order',  'created_timestamp', 'update_timestamp')
+	readonly_fields = ('created_timestamp', 'update_timestamp')
+	extra = 0
