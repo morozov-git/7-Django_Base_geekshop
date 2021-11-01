bdcaeda8f1eff89665bb13312b4129c2d5dcba68
morozov.da <morozov.da@gmail.com>
2021-11-01 05:43:11 +0300

    admin

--- users/admin.py
+++ users/admin.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 from .models import User
 from baskets.models import Basket
 from baskets.admin import BasketAdmin
-from ordersapp.models import Order
+
 # Register your models here.
 # admin.site.register(User)
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-	# model = Basket
-	# inlines = (BasketAdmin, )
-	model = Order
-	inlines = (Order, )
+	model = Basket
+	inlines = (BasketAdmin, )
